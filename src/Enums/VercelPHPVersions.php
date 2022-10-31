34ef0c97928475dfa2d58475eaf9770eebe3aa51
Mridhul <mridhulka@gmail.com>
2022-10-31 20:22:46 +0530

    bump vercel runtime version

--- src/Enums/VercelPHPVersions.php
+++ src/Enums/VercelPHPVersions.php
@@ -4,7 +4,7 @@ namespace Mridhulka\LaravelVercel\Enums;
 
 enum VercelPHPVersions: string
 {
-    case PHP81 = 'vercel-php@0.5.1 - PHP 8.1.x';
+    case PHP81 = 'vercel-php@0.5.2 - PHP 8.1.x';
     case PHP8 = 'vercel-php@0.4.0 - PHP 8.0.x';
     case PHP74 = 'vercel-php@0.3.2 - PHP 7.4.x';
 }
