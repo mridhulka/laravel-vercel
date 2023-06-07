cfa281049471285a571e58a6b200371b9b53eee6
Mridhul <mridhulka@gmail.com>
2023-06-08 01:02:18 +0530

    add & update version numbers

--- src/Enums/VercelPHPVersions.php
+++ src/Enums/VercelPHPVersions.php
@@ -4,7 +4,8 @@ namespace Mridhulka\LaravelVercel\Enums;
 
 enum VercelPHPVersions: string
 {
-    case PHP81 = 'vercel-php@0.5.2 - PHP 8.1.x';
-    case PHP8 = 'vercel-php@0.4.0 - PHP 8.0.x';
-    case PHP74 = 'vercel-php@0.3.2 - PHP 7.4.x';
+    case PHP82 = 'vercel-php@0.6.0 - PHP 8.2.x';
+    case PHP81 = 'vercel-php@0.5.3 - PHP 8.1.x';
+    case PHP8 = 'vercel-php@0.4.1 - PHP 8.0.x';
+    case PHP74 = 'vercel-php@0.3.3 - PHP 7.4.x';
 }
